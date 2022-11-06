158c7cff587f28443ca71df6fa7bb83c0382ec81
JG-C3 <jgc3django@gmail.com>
2022-11-07 02:28:48 +0900

    Django W9

---
+++ account/views.py
@@ -0,0 +1,39 @@
+# [코드 추가] redirect 추가
+from django.shortcuts import render, redirect
+# [코드 추가] UserCreationForm, AuthenticationForm 추가
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+# [코드 추가] login 함수를 auth_login 으로, logout 함수를 auth_logout 으로 추가
+from django.contrib.auth import login as auth_login, logout as auth_logout
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('page:index')
+    else:
+        form = UserCreationForm()
+    context = {
+        'type': '회원가입',
+        'form': form,
+    }
+    return render(request, 'account/form.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('page:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'type': '로그인',
+        'form': form,
+    }
+    return render(request, 'account/form.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('page:index')
---
+++ page/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'page'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create/', views.posting_create, name='posting_create'),
+    path('list/', views.posting_list, name='posting_list'),
+    path('<int:posting_id>/', views.posting_detail, name='posting_detail'),
+    path('<int:posting_id>/update/', views.posting_update, name='posting_update'),
+    path('<int:posting_id>/delete/', views.posting_delete, name='posting_delete'),
+]
