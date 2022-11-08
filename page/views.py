09eebd7b378385600278d5b55c7bf1b4e8b11e86
JG-C3 <jgc3django@gmail.com>
2022-11-08 19:53:18 +0900

    Update Django W9

--- page/views.py
+++ page/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Posting, Comment
 from .forms import PostingForm, CommentForm
-# [코드 작성] login_required 데코레이션 추가
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -18,27 +16,20 @@ def posting_list(request):
 
 # [Create] 글 작성하기
 def posting_create(request):
-    # [코드 작성] 웹 페이지에 로그인이 되어있는 경우만 글 쓰기가 가능하도록 조건문 작성
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            posting_form = PostingForm(request.POST)
-            
-            if posting_form.is_valid():
-                # [코드 작성] posting_form에 작성자 객체 추가
-                posting_form = posting_form.save(commit=False)
-                posting_form.author = request.user
-                posting_form.save()
-                return redirect('page:posting_list')
-        else:
-            posting_form = PostingForm()
+    if request.method == 'POST':
+        posting_form = PostingForm(request.POST)
         
-        context = {
-            'posting_type': '글쓰기',
-            'posting_form': posting_form,
-        }
-        return render(request, 'page/posting_form.html', context)
-    # [코드 작성] 로그인이 되어있지 않은 경우 로그인 페이지로 돌아가도록 처리
-    return redirect('account:login')
+        if posting_form.is_valid():
+            posting_form.save()
+            return redirect('page:posting_list')
+    else:
+        posting_form = PostingForm()
+    
+    context = {
+        'posting_type': '글쓰기',
+        'posting_form': posting_form,
+    }
+    return render(request, 'page/posting_form.html', context)
 
 # [Read & Create] 작성글 보기 & 댓글 작성
 def posting_detail(request, posting_id):
@@ -50,8 +41,6 @@ def posting_detail(request, posting_id):
         if comment_form.is_valid():
             comment_form = comment_form.save(commit=False)
             comment_form.posting = posting
-            # [코드 작성] comment_form에 작성자 객체 추가
-            comment_form.author = request.user
             comment_form.save()
             return redirect('page:posting_detail', posting_id)
     else :
@@ -67,33 +56,25 @@ def posting_detail(request, posting_id):
     return render(request, 'page/posting_detail.html', context)
 
 # [Update] 작성글 수정
-# [코드 작성] login_required 데코레이션 추가
-@login_required
 def posting_update(request, posting_id):
     posting = get_object_or_404(Posting, id=posting_id)
-    # [코드 작성] 글 작성자가 로그인한 사람과 같을 경우에만 글 수정이 가능하도록 조건문 작성
-    if posting.author == request.user:
-        if request.method == 'POST':
-            posting_form = PostingForm(request.POST, instance=posting)
+    if request.method == 'POST':
+        posting_form = PostingForm(request.POST, instance=posting)
 
-            if posting_form.is_valid():
-                posting_form.save()
-                return redirect('page:posting_detail', posting_id)
-        else:
-            posting_form = PostingForm(instance=posting)
+        if posting_form.is_valid():
+            posting_form.save()
+            return redirect('page:posting_detail', posting_id)
+    else:
+        posting_form = PostingForm(instance=posting)
 
-        context = {
-            'posting_type': '글수정',
-            'posting': posting,
-            'posting_form': posting_form,
-        }
-        return render(request, 'page/posting_form.html', context)
-    # [코드 추가] posting_id에 해당하는 페이지로 redirect
-    return redirect('page:posting_detail', posting_id)
+    context = {
+        'posting_type': '글수정',
+        'posting': posting,
+        'posting_form': posting_form,
+    }
+    return render(request, 'page/posting_form.html', context)
 
 # [Delete] 작성글 삭제
-# [코드 작성] login_required 데코레이션 추가
-@login_required
 def posting_delete(request, posting_id):
     if request.method == 'POST':
         posting = get_object_or_404(Posting, id=posting_id)
@@ -102,8 +83,6 @@ def posting_delete(request, posting_id):
     return redirect('page:posting_detail', posting_id)
 
 # [Delete] 댓글 삭제
-# [코드 작성] login_required 데코레이션 추가
-@login_required
 def comment_delete(request, posting_id, comment_id):
     if request.method == 'POST':
         comment = get_object_or_404(Comment, id=comment_id)
